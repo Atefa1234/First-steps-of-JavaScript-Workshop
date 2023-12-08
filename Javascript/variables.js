879e051b45f35a2221525b3786307d1067f3a736
atefa1234 <atefa.mahmoodi2021@gmail.com>
2023-12-08 21:50:41 +0100

    variables finished

---
+++ Javascript/variables.js
@@ -0,0 +1,37 @@
+//variables data types
+// numbers
+let Number = 5;
+console.log(Number);
+document.write(Number+ '<br>');
+// strings
+let mySelf = "Atefa"
+console.log(mySelf);
+document.write(mySelf +'<br>');
+
+let myBrothers= ["Najib", "Aziz", "Habib", "AliReza", "MohammadReza"]
+console.log(myBrothers);
+document.write(myBrothers +'<br>');
+// boolean
+let a = 2;
+let b = 3;
+let c = 2;
+console.log(a==c);
+console.log(a==b);
+document.write((a==c) +'<br>');
+document.write((a==b) +'<br>');
+// numbers should do basics mathematics operations
+let z = a+b ;
+let y = a*b+c;
+let x = Math.floor(a+c/b);
+console.log(z);
+console.log(y);
+console.log(x);
+document.write(z +'<br>');
+document.write(y +'<br>');
+document.write(x +'<br>');
+//Concatenate the string with the ARRAY
+let myFamily = myBrothers.concat(mySelf);
+console.log(myFamily);
+document.write(myFamily +'<br>');
+
+
